850c37ee2e3a7d44d9a01e84bbe9f4237586c65e
SANTANATILAK <tilaksontana59@gmail.com>
2026-02-27 08:38:33 +0000

    Add setup.sh for Streamlit Cloud deployment

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.streamlit/
+echo "\
+[theme]\n\
+primaryColor = '#FF1493'\n\
+backgroundColor = '#0E1117'\n\
+secondaryBackgroundColor = '#262730'\n\
+textColor = '#FAFAFA'\n\
+font = 'sans serif'\n\
+" > ~/.streamlit/config.toml
